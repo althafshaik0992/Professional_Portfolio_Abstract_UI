b6ae2d2253f7e8ff0b2228d1820d3d7d52871531
AlthafMurfad <althafmurfad@althafmrfadsair.mynetworksettings.com>
2026-05-18 21:44:31 -0400

    added github page coded

--- Professional_Portfolio_Abstract_UI/src/main/java/com/althaf/portfolio/repository/ContactMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.althaf.portfolio.repository;
-
-import com.althaf.portfolio.model.ContactMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
-}
